25045d9d602f354e067783d9e4a39c413db8ad61
Sergio Villén Galera <sergio.villen@gmail.com>
2023-02-18 11:13:47 +0100

    Calling create and poll searches

--- FlightsAvailability.Search.Agent.Skyscanner/IntegrationEvents/EventHandling/FlightSearchQueryReceivedEventHandler.cs
+++ FlightsAvailability.Search.Agent.Skyscanner/IntegrationEvents/EventHandling/FlightSearchQueryReceivedEventHandler.cs
@@ -1,14 +1,17 @@
 ﻿using Dapr.Client;
 using EventBus.Abstractions;
+using FlightsAvailability.Search.Agent.Skyscanner.IntegrationEvents.Entities;
 using FlightsAvailability.Search.Agent.Skyscanner.IntegrationEvents.Events;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Text.Json;
 using System.Xml.Linq;
 
 namespace FlightsAvailability.Search.Agent.Skyscanner.IntegrationEvents.EventHandling
 {
     public class FlightSearchQueryReceivedEventHandler : IIntegrationEventHandler<FlightSearchQueryReceivedEvent>
     {
-        private const string DAPR_BINDING_NAME = "flights-availability-binding-skyscanner";
+        private const string DAPR_BINDING_SKYSCANNER_SEARCH_CREATE = "skyscanner-search-create";
+        private const string DAPR_BINDING_SKYSCANNER_SEARCH_POLL = "skyscanner-search-poll";
         private readonly IEventBus _eventBus;
         private readonly ILogger _logger;
         private readonly DaprClient _daprClient;
@@ -23,14 +26,44 @@ namespace FlightsAvailability.Search.Agent.Skyscanner.IntegrationEvents.EventHan
             _daprClient = daprClient;
         }
         public async Task Handle(FlightSearchQueryReceivedEvent @event)
-        {
+        {            
             var metadata = new Dictionary<string, string>();
-            metadata.Add("X-api-key", "prtl6749387986743898559646983194");            
-            var res = await _daprClient.InvokeBindingAsync<string,dynamic>(DAPR_BINDING_NAME, "get",string.Empty, (IReadOnlyDictionary<string, string>) metadata);
+            metadata.Add("X-api-key", "prtl6749387986743898559646983194");
+            var data = new { query = 
+                new {
+                    market = "ES",
+		            locale = "es-ES",
+		            currency = "EUR",
+                    adults = 1,
+		            cabin_class = "CABIN_CLASS_ECONOMY",
+                    query_legs = new List<object>()
+                    {
+                        new { 
+                            origin_place_id = new { iata = "MAD"},
+                            destination_place_id = new { iata = "CDG"},
+                            date = new { year = 2023, month = 6,day = 22 }
+                            }
+                    }
+                } 
+            };
+            var searchCreateResponse = await _daprClient.InvokeBindingAsync<object, dynamic>(DAPR_BINDING_SKYSCANNER_SEARCH_CREATE, "post", data, (IReadOnlyDictionary<string, string>) metadata);
 
-            var request = new BindingRequest(DAPR_BINDING_NAME, "get");
-            request.Metadata.Add("X-api-key", "prtl6749387986743898559646983194");
-            var response = await _daprClient.InvokeBindingAsync(request);
+            var response = JsonSerializer.Deserialize<SkyscannerResponse>(searchCreateResponse.ToString())!;
+            if (response != null && response.status == "RESULT_STATUS_INCOMPLETE")
+            {
+                metadata.Add("path", response.sessionToken);
+                var responseStatus = response.status;
+                while (responseStatus == "RESULT_STATUS_INCOMPLETE")
+                {
+                    var searchPollResponse = await _daprClient.InvokeBindingAsync<object, dynamic>(DAPR_BINDING_SKYSCANNER_SEARCH_POLL, "post", string.Empty, (IReadOnlyDictionary<string, string>)metadata);
+                    var pollResponse = JsonSerializer.Deserialize<SkyscannerResponse>(searchPollResponse.ToString())!;
+                    responseStatus = pollResponse.status;
+                }
+            }
+            //var request = new BindingRequest(DAPR_BINDING_SKYSCANNER_SEARCH_CREATE, "post");
+            //request.Metadata.Add("X-api-key", "prtl6749387986743898559646983194");
+            //request.Data = data;
+            //var response = await _daprClient.InvokeBindingAsync(request);
 
             await Task.Delay(3000);
         }
