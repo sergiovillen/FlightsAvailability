e019686d2f5b4f621d026b6d77815dafcc9e5ba1
Sergio Villén Galera <sergio.villen@gmail.com>
2023-02-20 12:21:53 +0100

    Getting raw results from Amadeus

--- FlightsAvailability.Search.Agent.Amadeus/IntegrationEvents/EventHandling/FlightSearchQueryReceivedEventHandler.cs
+++ FlightsAvailability.Search.Agent.Amadeus/IntegrationEvents/EventHandling/FlightSearchQueryReceivedEventHandler.cs
@@ -6,13 +6,18 @@ using Grpc.Core;
 using System.Text;
 using System;
 using System.Text.Json;
+using FlightsAvailability.Search.Agent.Amadeus.Entities;
+using static Google.Rpc.Context.AttributeContext.Types;
 
 namespace FlightsAvailability.Search.Agent.Amadeus.IntegrationEvents.EventHandling
 {
     public class FlightSearchQueryReceivedEventHandler : IIntegrationEventHandler<FlightSearchQueryReceivedEvent>
     {
+        private const string DAPR_BINDING_AMADEUS_GET_TOKEN = "amadeus-token";
+        private const string DAPR_BINDING_AMADEUS_FLIGHT_OFFERS = "amadeus-flight-offers";
         private const string SECRET_STORE_NAME = "flights-availability-secretstore";
         private const string AMADEUS_API_CLIENT_CREDENTIALS_SECRET_NAME = "amaudeus-api-client-credentials";
+        private const string AMADEUS_API_CONTENT_TYPE = "application/x-www-form-urlencoded";
         private readonly IEventBus _eventBus;
         private readonly ILogger _logger;
         private readonly DaprClient _daprClient;
@@ -29,14 +34,19 @@ namespace FlightsAvailability.Search.Agent.Amadeus.IntegrationEvents.EventHandli
         public async Task Handle(FlightSearchQueryReceivedEvent @event)
         {
             var clientCredentials = await _daprClient.GetSecretAsync(SECRET_STORE_NAME, AMADEUS_API_CLIENT_CREDENTIALS_SECRET_NAME);
-            var metadata = new Dictionary<string, string>();
-            metadata.Add("Content-Type", "application/x-www-form-urlencoded");
             string data = $"grant_type=client_credentials&client_id={clientCredentials["client_id"]}&client_secret={clientCredentials["client_secret"]}";
-            var req = new BindingRequest("amadeus-token", "post");
-            req.Metadata.Add("Content-Type", "application/x-www-form-urlencoded");
+            var req = new BindingRequest(DAPR_BINDING_AMADEUS_GET_TOKEN, "post");
+            req.Metadata.Add("Content-Type", AMADEUS_API_CONTENT_TYPE);
             req.Data = new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes(data));
-            var response = await _daprClient.InvokeBindingAsync(req);
-            var json = Encoding.UTF8.GetString(response.Data.Span);
+            var bindingResponse = await _daprClient.InvokeBindingAsync(req);
+            var amadeusToken = JsonSerializer.Deserialize<AmadeusTokenResponse>(Encoding.UTF8.GetString(bindingResponse.Data.Span))!;
+
+            //Calling Flight Offers
+            var metadata = new Dictionary<string, string>();
+            metadata.Add("Authorization", $"Bearer {amadeusToken.access_token}");
+            metadata.Add("path", "/flight-offers?originLocationCode=MAD&destinationLocationCode=CDG&departureDate=2023-05-02&adults=1&nonStop=false&max=250");
+            var searchPollResponse = await _daprClient.InvokeBindingAsync<object, dynamic>(DAPR_BINDING_AMADEUS_FLIGHT_OFFERS, "get", string.Empty, (IReadOnlyDictionary<string, string>)metadata);
+
         }
     }
 }
