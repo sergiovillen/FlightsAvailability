1c64d5cf6f433e9cfb690fc138b1cd8ee176e9de
Sergio Villén Galera <sergio.villen@gmail.com>
2023-02-20 10:24:38 +0100

    Calling amadeus to get token

--- FlightsAvailability.Search.Agent.Amadeus/IntegrationEvents/EventHandling/FlightSearchQueryReceivedEventHandler.cs
+++ FlightsAvailability.Search.Agent.Amadeus/IntegrationEvents/EventHandling/FlightSearchQueryReceivedEventHandler.cs
@@ -1,6 +1,10 @@
 ﻿using Dapr.Client;
 using EventBus.Abstractions;
 using FlightsAvailability.Search.Agent.Amadeus.IntegrationEvents.Events;
+using Google.Type;
+using Grpc.Core;
+using System.Text;
+using System;
 using System.Text.Json;
 
 namespace FlightsAvailability.Search.Agent.Amadeus.IntegrationEvents.EventHandling
@@ -24,7 +28,15 @@ namespace FlightsAvailability.Search.Agent.Amadeus.IntegrationEvents.EventHandli
         }
         public async Task Handle(FlightSearchQueryReceivedEvent @event)
         {
-            var clientId = await _daprClient.GetSecretAsync(SECRET_STORE_NAME, AMADEUS_API_CLIENT_CREDENTIALS_SECRET_NAME);
+            var clientCredentials = await _daprClient.GetSecretAsync(SECRET_STORE_NAME, AMADEUS_API_CLIENT_CREDENTIALS_SECRET_NAME);
+            var metadata = new Dictionary<string, string>();
+            metadata.Add("Content-Type", "application/x-www-form-urlencoded");
+            string data = $"grant_type=client_credentials&client_id={clientCredentials["client_id"]}&client_secret={clientCredentials["client_secret"]}";
+            var req = new BindingRequest("amadeus-token", "post");
+            req.Metadata.Add("Content-Type", "application/x-www-form-urlencoded");
+            req.Data = new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes(data));
+            var response = await _daprClient.InvokeBindingAsync(req);
+            var json = Encoding.UTF8.GetString(response.Data.Span);
         }
     }
 }
